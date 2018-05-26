256f633af9142fcb3fd5f93590ded7d696fa830d
m0wong <m0wongxx@gmail.com>
2018-05-26 13:59:35 +0800

    20180525

---
+++ 7 Kyu/Sum of all the multiples of 3 or 5.js
@@ -0,0 +1,26 @@
+/*
+Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+*/
+
+function findSum(n) {
+  const f = parseInt(n/5);
+  const t = parseInt(n/3);
+  const ft = parseInt(n/15);
+  return 5*sum(f) + 3*sum(t) - 15*sum(ft);
+}
+function sum(n){
+  if(n === 0){
+     return 0;
+  }else{
+     return n + sum(n-1);
+  }
+}
+
